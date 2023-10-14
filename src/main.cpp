7317d8016c14bc2f141782f98d6e2c61022a9bd2
MuhXd <momo2010sofia@gmail.com>
2023-10-14 15:01:09 +0100

    NEW CPP CODE

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,9 @@
 #include <Geode/Geode.hpp>
+#include <Geode/modify/MenuLayer.hpp>
+#include <Geode/modify/CreatorLayer.hpp>
+#include <Geode/binding/EndLevelLayer.hpp>
+#include <Geode/cocos/base_nodes/Layout.hpp> 
+
+using namespace geode::prelude;
 // ADD ALL HOOK FILES HERE
 #include "menu.cpp"
--- src/menu.cpp
+++ src/menu.cpp
@@ -1,10 +1,4 @@
-#include <Geode/Geode.hpp>
-#include <Geode/modify/MenuLayer.hpp>
-#include <Geode/modify/CreatorLayer.hpp>
-#include <Geode/binding/EndLevelLayer.hpp>
-#include <Geode/cocos/base_nodes/Layout.hpp> 
 
-using namespace geode::prelude;
 auto Mainbooter = true;
 auto FirstBoot = false;
 
