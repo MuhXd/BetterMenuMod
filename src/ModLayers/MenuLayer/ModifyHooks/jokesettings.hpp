bce4f6f8b2bab53f3f5863ce8e94a24bd9602047
Alphalaneous <38200084+Alphalaneous@users.noreply.github.com>
2024-06-23 01:07:15 -0400

    Update jokesettings.hpp

--- src/ModLayers/MenuLayer/ModifyHooks/jokesettings.hpp
+++ src/ModLayers/MenuLayer/ModifyHooks/jokesettings.hpp
@@ -2,8 +2,8 @@
 #include <Geode/Geode.hpp>
 #include <UIBuilder.hpp>
 #include <Geode/ui/GeodeUI.hpp>
-using namespace geode::prelude;
 
+using namespace geode::prelude;
 
 void PlaySoundEffect(auto effect){
 	if (Mod::get()->getSettingValue<bool>("SFX")) {
@@ -21,33 +21,34 @@ void onGeode(CCObject*) {
 
 static void JokesMain(auto layer) {
 
- if (Mod::get()->getSettingValue<bool>("Uwugeode")) {
+ 	if (Mod::get()->getSettingValue<bool>("Uwugeode")) {
         auto geodeButton = dynamic_cast<CCMenuItemSpriteExtra*>(
 		layer->getChildByIDRecursive("geode.loader/geode-button")
 	    );
-		geodeButton->m_pfnSelector = menu_selector(FakeLayer::onGeode);
-		 for(auto items : CCArrayExt<CCNode*>(layer->getChildByIDRecursive("geode.loader/geode-button")->getChildren())) {
-			auto g=items;
-			if (auto icon = getChildOfType<CCSprite>(g, 0)) {
-        	auto Spr = CCSprite::create("gwode.png"_spr);
-			Spr->setScale(0.2f);
-			Spr->setPositionX(icon->getPositionX());
-			Spr->setPositionY(icon->getPositionY());
-			g->addChild(Spr);
-			icon->setVisible(false);
-			break;
-    	    };
-		 }
-         };
 
-        if (Mod::get()->getSettingValue<bool>("Merp")) {
-            auto title = layer->getChildByID("main-title");
-            auto titlePos = title->getPosition();
-            auto Spr = CCSprite::create("MERP.png"_spr);
-            Spr->setPositionX(titlePos.x);
-            Spr->setPositionY(titlePos.y);
-            layer->removeChildByID("main-title");
-            Spr->setID("main-title");
-            layer->addChild(Spr);
-        }
+		geodeButton->m_pfnSelector = menu_selector(FakeLayer::onGeode);
+		for(auto items : CCArrayExt<CCNode*>(layer->getChildByIDRecursive("geode.loader/geode-button")->getChildren())) {
+			auto g=items;
+				if (auto icon = getChildOfType<CCSprite>(g, 0)) {
+				auto Spr = CCSprite::create("gwode.png"_spr);
+				Spr->setScale(0.2f);
+				Spr->setPositionX(icon->getPositionX());
+				Spr->setPositionY(icon->getPositionY());
+				g->addChild(Spr);
+				icon->setVisible(false);
+				break;
+    	    };
+		}
+	};
+
+	if (Mod::get()->getSettingValue<bool>("Merp")) {
+		auto title = layer->getChildByID("main-title");
+		auto titlePos = title->getPosition();
+		auto Spr = CCSprite::create("MERP.png"_spr);
+		Spr->setPositionX(titlePos.x);
+		Spr->setPositionY(titlePos.y);
+		layer->removeChildByID("main-title");
+		Spr->setID("main-title");
+		layer->addChild(Spr);
+	}
 }
